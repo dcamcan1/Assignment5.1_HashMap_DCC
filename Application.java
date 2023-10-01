1c62f39e14f024acfc9b33e008d3fc3a3851aad3
dcamcan1 <daniel.ccan@gmail.com>
2023-09-30 21:01:27 -0400

    Initial Commit

---
+++ Application.java
@@ -0,0 +1,29 @@
+import java.io.FileNotFoundException;
+
+/** Driver to test Dictionary class.
+ * 
+ *  @author Danny Cannon
+ */
+public class Application {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		/** Creates dicitonary. */
+		Dictionary diction = new Dictionary();
+		
+		/** Tests load method. */
+		diction.load(); 
+		
+		/** Tests search method with 5 different words. */
+		System.out.println(diction.search("job"));
+		System.out.println(diction.search("rude"));
+		System.out.println(diction.search("sore"));
+		System.out.println(diction.search("knave"));
+		System.out.println(diction.search("visionary"));
+		System.out.println(diction.search("visiwerffe")); //Testing null value
+		System.out.println("\n");
+		/** Tests print method. */
+		diction.printD();
+	}
+
+}
---
+++ Dictionary.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+/** Represents a dictionary containing archaic words
+ *  and their definitions.
+ *  
+ *  @author Danny Cannon
+ */
+public class Dictionary {
+
+	//Instance Variable
+	private HashMap<String, String> wordMap;
+	
+	/** Creates a map of the words. */
+	public Dictionary() {
+		wordMap = new HashMap<>();
+	}//end constructor
+	
+	//Methods
+	/** Loads the unsorted key/definition pairs into the HashMap.
+	 *  @throws FileNotFoundException. */
+	public void load() throws FileNotFoundException  {
+		Scanner scan = new Scanner(new File("DictionaryWordValuePairs.csv"));
+		while(scan.hasNextLine()) {
+			String values[] = scan.nextLine().split(",");
+			wordMap.put(values[0], values[1]);
+		}//end while
+		scan.close();
+	}//end loadDictionary
+	
+	/** Prints the word, definition, and HashKey used for the word by the HashMap. */
+	public void printD() {
+		for (Map.Entry<String, String> entry : wordMap.entrySet()) {
+			System.out.println(entry.getKey() + ": " + entry.getValue());
+		}//end for
+	}//end printDictionary
+	
+	/** Searches the dictionary for a key. 
+	 * 	@param word  key to search for in the dictionary. */
+	public String search(String word) {
+		if (wordMap.get(word) == null) {
+			return "\"" + word + "\"" + " does not exist in this dictionary.";
+		}
+		else {
+			return word + ": " + wordMap.get(word) + " (hashCode: " + word.hashCode() + ")";
+		}
+	}//end search
+
+	
+}//end Dictionary class
